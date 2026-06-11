f608a175e36c698a9e61c71a77d80d68a6763595
MJInventory <toebab@gmail.com>
2026-06-11 12:13:33 -0400

    v114 web push notifications for phone app - public/dashboard.js

--- public/dashboard.js
+++ public/dashboard.js
@@ -83,6 +83,7 @@ function saveSession(data) {
   localStorage.setItem("kitchenStockPermissions", JSON.stringify(sessionPermissions));
   localStorage.setItem("kitchenStockTheme", data.user.theme || "dark");
   window.applyKitchenTheme?.(data.user.theme || "dark");
+  window.setupKitchenPush?.();
 }
 
 function showApp() {

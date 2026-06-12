da778fb35b4ba530d4ab02ac26bb886e564d0ab6
MJInventory <toebab@gmail.com>
2026-06-11 23:20:32 -0400

    Deploy MJ Stock Magic v126 - public/sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "kitchen-stock-v125";
+const CACHE_NAME = "kitchen-stock-v126";
 const APP_SHELL = [
   "/",
   "/index.html",
@@ -147,6 +147,7 @@ self.addEventListener("notificationclick", (event) => {
     await clients.openWindow(targetUrl);
   })());
 });
+
 
 
 

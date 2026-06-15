84e6d2fe9305f597a6de7d648f116fbf95473584
MJInventory <toebab@gmail.com>
2026-06-15 10:47:36 -0400

    Deploy MJ Stock Magic v134 driver qty fix

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "kitchen-stock-v133";
+const CACHE_NAME = "kitchen-stock-v134";
 const APP_SHELL = [
   "/",
   "/index.html",
@@ -147,6 +147,7 @@ self.addEventListener("notificationclick", (event) => {
     await clients.openWindow(targetUrl);
   })());
 });
+
 
 
 

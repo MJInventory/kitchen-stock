08fcfcda365666dad5f1306f75a554529d9f1c68
MJInventory <toebab@gmail.com>
2026-06-16 11:25:50 -0400

    Deploy MJ Stock Magic v157 fix mobile ordering selected-row layout - public/theme.js

--- public/theme.js
+++ public/theme.js
@@ -1,5 +1,5 @@
 ﻿(function () {
-  const APP_VERSION = "v156";
+  const APP_VERSION = "v157";
 
   function applyTheme(theme) {
     const normalized = theme === "light" ? "light" : "dark";
@@ -57,6 +57,7 @@
   applyTheme(localStorage.getItem("kitchenStockTheme") || "dark");
   registerUpdater();
 }());
+
 
 
 
